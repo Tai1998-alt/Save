bb8ff9b9a42c7c713f7ab0ca40aa98d5bc7e2e16
ADMIN <speakerstai@gmail.com>
2021-03-30 23:26:41 +0700

    LeVanTai_18093421

---
+++ app/src/main/java/com/example/a18093421_levantai_bupi2/Layout2.java
@@ -0,0 +1,14 @@
+package com.example.a18093421_levantai_bupi2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout2);
+    }
+}
